76e97491555a340b893dc6ec7239de2e8e7bda5a
Vinare <scrat-me@mail.ru>
2022-02-23 22:50:18 +0300

    Done

--- script.js
+++ script.js
@@ -1,29 +1,28 @@
 "use strict";
 
-let rollback = 5;
-const title = prompt('Как называется ваш проект?');
-const screens = prompt('Какие типы экранов нужно разработать?', "Простые, Сложные, Интерактивные");
-const screenPrice = prompt('Сколько будет стоить данная работа?', '12000 руб.');
-const adaptive = confirm('Нужен ли адаптив на сайте?');
-const service1 = prompt('Какой дополнительный тип услуги нужен?');
-const servicePrice1 = prompt('Сколько это будет стоить?');
-const service2 = prompt('Какой дополнительный тип услуги нужен?');
-const servicePrice2 = prompt('Сколько это будет стоить?');
-const fullPrice = parseInt(screenPrice) + parseInt(servicePrice1) + parseInt(servicePrice2);
-const servicePercentPrice = Math.ceil(fullPrice - (fullPrice * rollback/100));
+class First {
+  constructor(name, age) {
+    this.name = name;
+    this.age = age;
+  }
 
-console.log(servicePercentPrice);
-
-if (fullPrice >= 30000) {
-  console.log('Даем скидку в 10%');
-} else if (fullPrice < 30000 && fullPrice >= 15000) {
-  console.log('Даем скидку в 5%');
-} else if (fullPrice < 15000 && fullPrice > 0) {
-  console.log('Скидка не предусмотрена');
-} else if (fullPrice <= 0) {
-  console.log('Что-то пошло не так');
+  hello() {
+    console.log('Привет, я метод родителя!');
+  }
 }
 
+const first = new First('Kate', 32);
 
+class Second extends First {
+  hello() {
+    super.hello();
+    console.log('А я наследуемый метод!');
+  }
+}
 
+const second = new Second('Kate', 32);
 
+second.hello();
+
+console.log(first);
+console.log(second);
